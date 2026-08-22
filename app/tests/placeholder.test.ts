8ea41433205523c259704a4a2f331a0998f24537
Ilya Kosobrukhov <100542385+ilya-kos@users.noreply.github.com>
2026-08-22 19:26:05 +1000

    Ci/initial ci setup (#2)
    * chore: installed dotenv package
    
    * chore: created basic placeholder test for vitest to run successfully
    
    * ci: created basic Github Actions workflow
    
    * chore: small change to .env.example
    
    * ci: enabled manual triggering of the workflow
    
    * ci: made build job upload artifact
    
    * ci: removed pnpm version from ci.yaml, should take from package.json
    
    * ci: updated actions to use newer pnpm install actions
    
    * ci: updated checkout action to latest version
    
    * ci: removed pnpm store caching
    
    * chore: updated .env.example
    
    * docs: updated README.md
    
    * docs: corrections
    
    * chore: minor adjustment to .env.example
    
    * docs: more corrections

---
+++ app/tests/placeholder.test.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest'
+
+// Placing here so that vitest has something to run
+// Should be removed once actual tests have been written
+test('true is true', () => [
+    expect(true).toBe(true)
+])
